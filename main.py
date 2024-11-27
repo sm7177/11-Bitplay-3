08d7e8cacff9bbde2efa3e6af74cd86a83012e9b
sm7177 <subrinamaharjan2020@gmail.com>
2024-11-27 18:51:14 +0545

    .

--- main.py
+++ main.py
@@ -47,3 +47,21 @@
 
 
 
+def divide(ourdividend,ourdivisor):
+    sign=(-1 if ((ourdividend <0)^(ourdivisor <0))else 1)
+    ourdividend=abs(ourdividend)
+    ourdivisor=abs(ourdivisor)
+    
+    quo=0
+    temp=0
+    for i in range(31, -1, -1):
+        if temp + (ourdivisor << i) <= ourdividend:
+            temp += ourdivisor << i
+            quo |= 1 << i
+    if sign ==-1:
+        quo=-quo
+    return quo
+a=int(input("Enter a number:"))
+b=int(input("Enter another number:"))
+
+print("The answer is",divide(a,b))
