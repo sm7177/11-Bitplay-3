8a115edb32411dd35e26965d7f729e9adfcd0ffa
sm7177 <subrinamaharjan2020@gmail.com>
2024-11-27 18:38:24 +0545

    .

---
+++ main.py
@@ -0,0 +1,49 @@
+# a=int(input("Enter a number:"))
+# b=int(input("Enter another number:"))
+
+# a=a+b
+# b=a-b
+# a=a-b
+
+# print("After swapping, the values are: a =",a, "and b =",b)
+
+
+
+
+
+
+
+
+# def swap(a,b):
+#     a=a^b
+#     b=a^b
+#     a=a^b
+#     print("After swapping, the values are: a =",a, "and b =",b)
+    
+# swap(2,3)
+
+
+
+
+
+
+
+
+# def swap(a,b):
+#     a=(a&b)+(a|b)
+#     b=a+(~b)+1
+#     a=a+(~b)+1
+
+#     print("After swapping, the values are: a =",a, "and b =",b)
+
+# swap(2,3)
+
+
+
+
+
+
+
+
+
+
